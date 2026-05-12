72af647b1be56e6ae8b11517c9e156ea0c599cb3
Frezzard2 <kukucska.zsombor@gmail.com>
2026-05-12 09:55:20 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Üdvözöllek a játékban!");
+        System.out.println("Ez egy szöveg alapú kaland játék, amiben egy izgalmas történeten járhatsz végig. Emellett a döntésed mind befolyásolják a cselekmény kimenetelét. Bölcsen válassz, hogy jó úton járj.");
+        System.out.println("A sztori során a kimeneteleket a CLI alján találod majd és 1-2-3-4 számok közül kell választanod a neked legjobban tetsző folytatást, kimenetelt a jelenleg előállt helyzetnek.");
+        System.out.println("--------------------------------------");
+        System.out.println("Nyomj Enter-t a folytatáshoz...");
+        input.nextLine();
+
+    
+
+    }
+}
